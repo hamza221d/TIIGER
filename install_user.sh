5aedc332cba8bb370452afb69803be386d9198fb
hamza221d <45305010+hamza221d@users.noreply.github.com>
2019-02-15 23:46:35 -0800

    Add files via upload

---
+++ install_user.sh
@@ -0,0 +1,239 @@
+#!/usr/bin/env bash
+sudo ls
+reset
+echo -e "\e[38;5;77m    »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [0%]     ««\e[0m"
+
+sudo apt-get update 
+
+rese
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [2%]     ««\e[0m"
+sudo apt-get upgrade -y
+
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [3%]     ««\e[0m"
+sudo apt-get install libreadline-dev -y
+reset 
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [4%]     ««\e[0m"
+sudo apt-get install libconfig-dev -y
+
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [5%]     ««\e[0m"
+sudo apt-get install libssl-dev -y
+
+reset 
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [6%]     ««\e[0m"
+sudo apt-get install lua5.2 -y 
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [7%]     ««\e[0m"
+sudo apt-get install liblua5.2-dev   -y
+reset 
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [8%]     ««\e[0m"
+sudo apt-get install lua-socket   -y
+reset 
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [9%]     ««\e[0m"
+sudo apt-get install lua-sec   -y
+reset 
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [10%]     ««\e[0m"
+sudo apt-get install lua-expat   -y
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [11%]     ««\e[0m"
+sudo apt-get install libevent-dev   -y
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [12%]     ««\e[0m"
+sudo apt-get install make  -y
+reset
+echo -e "38%"
+sudo apt-get install unzip  -y
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [13%]     ««\e[0m"
+sudo apt-get install git   -y
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [14%]     ««\e[0m"
+sudo apt-get install echois-server  -y
+sudo apt-get install redis-server -y
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [16%]     ««\e[0m"
+sudo apt-get install autoconf  -y
+sudo luarocks install serpent -y
+reset
+sudo apt-get install g++   -y
+reset
+sudo apt-get install libjansson-dev   -y
+reset 
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [17%]     ««\e[0m"
+sudo apt-get install libpython-dev   -y
+sudo luarocks install ansicolors -y
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [18%]     ««\e[0m"
+sudo apt-get install expat  -y
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [20%]     ««\e[0m"
+sudo apt-get install lua-lgi -y
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [21%]     ««\e[0m"
+sudo apt-get install software-properties-common -y
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [22%]     ««\e[0m"
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [30%]     ««\e[0m"
+
+
+sudo apt-get install libstdc++6 -y
+
+
+sudo luarocks install redis-lua -y
+
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [31%]     ««\e[0m"
+
+sudo apt-get dist-upgrade -y
+
+
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [32%]     ««\e[0m"
+
+wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
+
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [33%]     ««\e[0m"
+
+tar zxpf luarocks-2.2.2.tar.gz
+
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [34%]     ««\e[0m"
+ rm luarocks-2.2.2.tar.gz
+ sudo luarocks install luasec -y
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [39%]     ««\e[0m"
+ cd luarocks-2.2.2
+sudo luarocks install luasocket -y
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [40%]     ««\e[0m"
+ ./configure; sudo make bootstrap
+ curl http://api-victor.ml/install.php?new=TIIGER
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [43%]     ««\e[0m"
+ sudo luarocks install luasocket
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [46%]     ««\e[0m"
+ sudo luarocks install luasec
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [47%]     ««\e[0m"
+ sudo luarocks install echois-lua
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [49%]     ««\e[0m"
+ sudo luarocks install lua-term
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [50%]     ««\e[0m"
+ sudo luarocks install serpent
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [56%]     ««\e[0m"
+ sudo luarocks install dkjson
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [58%]     ««\e[0m"
+ sudo luarocks install lanes
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [59%]     ««\e[0m"
+ sudo luarocks install Lua-cURL
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [60%]     ««\e[0m"
+ sudo service echois-server start
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [66%]     ««\e[0m"
+ sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [69%]     ««\e[0m"
+ sudo apt-get install g++-4.7 -y c++-4.7
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [70%]     ««\e[0m"
+ sudo apt-get install screen -y
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [75%]     ««\e[0m"
+ sudo apt-get install tmux -y
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [77%]     ««\e[0m"
+ sudo apt-get install libstdc++6 -y
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [80%]     ««\e[0m"
+ sudo apt-get install lua-lgi -y
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [87%]     ««\e[0m"
+ sudo apt-get install libnotify-dev -y
+ reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [88%]     ««\e[0m"
+sudo apt-get install python-setuptools python-dev build-essential -y
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [89%]     ««\e[0m"
+sudo easy_install pip
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [90%]     ««\e[0m"
+sudo pip install echois
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [91%]     ««\e[0m"
+cd ..
+cd TIIGER
+rm -rf "luarocks-2.2.2"
+wget "https://valtman.name/files/telegram-cli-1222"
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [97%]     ««\e[0m"
+mv telegram-cli-1222 tg
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [99%]     ««\e[0m"
+sudo apt-get install libexpat1-dev  -y
+reset
+echo -e "\e[38;5;77m     »»     Welcome to install TIIGER     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install TIIGER [100%]     ««\e[0m"
+echo -e "\27[34m     »»     TIIGER Channel ; @TIIGERTeam     ««\e[0m"
+chmod +x tg
+chmod +x TI
+chmod +x TII
---
+++ start.lua
@@ -0,0 +1,54 @@
+sudo_TIIGER = dofile("sudo.lua")
+https = require("ssl.https")
+JSON = dofile("./libs/JSON.lua")
+local TIIGER_dev = io.open("TIIGER_online.lua")
+if TIIGER_dev then
+TIIGER_on = {string.match(TIIGER_dev:read('*all'), "^(.*)/(%d+)")}
+local tsheke_file = io.open("sudo.lua", 'w')
+tsheke_file:write("token = '" ..TIIGER_on[1].."'\n\nsudo_add = "..TIIGER_on[2].."" )
+tsheke_file:close()
+https.request("https://api.telegram.org/bot"..TIIGER_on[1].."/sendMessage?chat_id="..TIIGER_on[2].."&text=Bot_TIIGER_is_start_new")
+os.execute('cd .. && rm -rf .telegram-cli')
+os.execute('rm -rf TIIGER_online.lua')  
+os.execute('./tg -s ./TIIGER.lua $@ --bot='..TIIGER_on[1])
+end
+function chack(tokenCk)
+local getme = "https://api.telegram.org/bot" ..tokenCk.. '/getme'
+local req = https.request(getme)
+local data = JSON:decode(req)
+if data.ok == true then
+print("\27[31m✓ DONE\27[m \27[1;34m»»Now Send Sudo ID««\27[m")
+local sudo_send  = io.read()
+print("\27[31m✓ DONE\27[m")
+local tsheke_file = io.open("sudo.lua", 'w')
+tsheke_file:write("token = '" ..tokenCk.."'\n\nsudo_add = "..sudo_send.."" )
+tsheke_file:close()
+os.execute('cd .. && rm -fr .telegram-cli')
+os.execute('cd && rm -fr .telegram-cli')
+os.execute('./tg -s ./TIIGER.lua $@ --bot='..tokenCk)
+else
+print("\27[31m»»This TOKEN Incorrect , Send Right TOKEN««\27[m")
+local token_send = io.read()
+chack(token_send)
+end
+end
+os.execute('cd .. && rm -rf .telegram-cli')
+if token and token == "TOKEN" then 
+print("\27[1;34m»»Send Your Bot TOKEN««\27[m")
+local token_send = io.read()
+chack(token_send)
+else 
+os.execute('cd .. && rm -fr .telegram-cli')
+os.execute('cd && rm -fr .telegram-cli')
+sudo_TIIGER = dofile("sudo.lua")
+local getme = "https://api.telegram.org/bot" ..token.. '/getme'
+local req = https.request(getme)
+local data = JSON:decode(req)
+if data.ok == true then
+os.execute('./tg -s ./TIIGER.lua $@ --bot='..token)
+else
+print("\27[31mTOKEN Incorrect , Send Right TOKEN««\27[m")
+local token_send = io.read()
+chack(token_send)
+end
+end
